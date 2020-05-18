f7d2e970949e5f99ac4de083d2bf99680e087f3a
iso0406@gmail.com <iso0406@gmail.com>
2020-05-18 20:31:33 -0300

    Teste da primeira classe de dominio

--- src/main/java/com/cursomc/resources/CategoriasResources.java
+++ src/main/java/com/cursomc/resources/CategoriasResources.java
@@ -1,18 +1,28 @@
 package com.cursomc.resources;
 
-import javax.swing.text.html.FormSubmitEvent.MethodType;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriasResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "Rest está Funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista =  new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
